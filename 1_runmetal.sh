6ebab79cfe3549c3d841a7fac2cb001692370295
yixuanh <54297194+yixuanh@users.noreply.github.com>
2025-03-31 16:51:20 -0500

    Update and rename runmetal.sh to 1_runmetal.sh

---
+++ 1_runmetal.sh
@@ -0,0 +1,2 @@
+module load gcc/6.2.0
+/METAL/build/bin/metal < 1_metal.txt #for east asian meta-analysis
--- runmetal.sh
+++
@@ -1,3 +0,0 @@
-module load gcc/6.2.0
-/METAL/build/bin/metal < METAL_flag_EAS.txt #for east asian meta-analysis
-#/METAL/build/bin/metal < METAL_flag_MULT.txt #for multi-ancestry meta-analysis
